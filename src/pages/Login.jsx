1699fafed1eface0eed26ee0b4a24ad27462601e
Hiruvindi <hiruvindi2000@gmail.com>
2024-11-01 08:45:36 +0530

    Finished user login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,98 +1,151 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
+import Loading from "../components/Loading";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const commonStyles = {
   input:
-    "w-full pl-4 p-2 rounded-full focus:outline-none focus:ring focus:ring-ring",
+    "w-full pl-4 text-x1 p-2 rounded-full focus:outline-none focus:ring focus:ring-ring",
   label: "block text-white text-muted-foreground mb-1",
   button:
     "bg-x3 text-primary-foreground font-bold rounded-full w-28 p-2 text-white rounded hover:bg-opacity-90",
 };
 
 const Login = () => {
-  const [userData, setUserData] = useState({
-    email: "",
-    password: "",
-  });
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-  //handle mail change
-  const handleEmailChange = (e) => {
-    setUserData({
-      ...userData,
-      email: e.target.value,
-    });
-  };
+  useEffect(() => {
+    // if (sessionStorage.getItem("role") !== "GUEST") {
+    //   window.alert("You are already logged in");
+    //   navigate("/");
+    // }
+  }, []);
 
-  //handle password change
-  const handlePasswordChange = (e) => {
-    setUserData({
-      ...userData,
-      password: e.target.value,
-    });
+  // Handle form submission
+  const handleSubmit = () => {
+    if (name != "" || password != "") {
+      setLoading(true);
+      axios
+        .post("http://localhost:8081/login", {
+          username: name,
+          password: password,
+        })
+        .then((res) => {
+          setLoading(false);
+          if (res.status == 200) {
+            let token = res.headers.authorization;
+            const decoded = jwtDecode(token);
+
+            console.log(decoded);
+
+            sessionStorage.setItem("SavedToken", token);
+            let userRole;
+
+            if (decoded.authorities.length == 4) {
+              userRole = decoded.authorities[2].authority;
+            }
+            if (decoded.authorities.length == 5) {
+              userRole = decoded.authorities[4].authority;
+            }
+
+            const mail = decoded.sub;
+            sessionStorage.setItem("role", userRole);
+            sessionStorage.setItem("email", mail);
+
+            axios.defaults.headers.common["Authorization"] = token;
+            if (userRole == "ROLE_USER") {
+              navigate("/search");
+            } else if (userRole == "ROLE_PHARMACIST") {
+              navigate("/data");
+            } else if (userRole == "ROLE_ADMIN") {
+              navigate("/admin");
+            }
+          }
+        })
+        .catch((e) => {
+          setLoading(false);
+          console.log(e);
+          window.alert("Password wrong!");
+        });
+    } else {
+      window.alert("Enter both Mail & Password");
+    }
   };
 
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <div className="flex justify-center bg-background my-8">
-        <div className="bg-card bg-x1 p-6 rounded-3xl shadow-lg w-96 max-w-md">
-          <img src="/images/logo.png" alt="MediCare" className="mx-auto h-36" />
-          <form>
-            <div className="mb-4 mt-8">
-              <label className={commonStyles.label} htmlFor="email">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                className={commonStyles.input}
-                placeholder="Enter your email here"
-                value={userData.email}
-                onChange={handleEmailChange}
-                required
-              />
+      <main className="flex-grow">
+        {loading && <Loading />}
+        <div className="flex justify-center bg-background my-8 mx-4">
+          <div className="bg-card bg-x1 p-6 rounded-3xl shadow-lg w-96 max-w-md">
+            <img
+              src="/images/logo.png"
+              alt="MediCare"
+              className="mx-auto h-36"
+            />
+            <div>
+              <div className="mb-4 mt-8">
+                <label className={commonStyles.label} htmlFor="email">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className={commonStyles.input}
+                  placeholder="Enter your email here"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="mb-4">
+                <label className={commonStyles.label} htmlFor="password">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  className={commonStyles.input}
+                  placeholder="Enter your password here"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="flex items-center justify-center mt-8">
+                <button
+                  onClick={handleSubmit}
+                  type="submit"
+                  className={commonStyles.button}
+                >
+                  LOGIN
+                </button>
+              </div>
             </div>
-            <div className="mb-4">
-              <label className={commonStyles.label} htmlFor="password">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                className={commonStyles.input}
-                placeholder="Enter your password here"
-                value={userData.password}
-                onChange={handlePasswordChange}
-                required
-              />
-            </div>
-            <div className="flex items-center justify-center mt-8">
-              <button
-                onClick={() => console.log(userData)}
-                type="submit"
-                className={commonStyles.button}
-              >
-                LOGIN
-              </button>
-            </div>
-          </form>
-          <p className="text-muted-foreground text-white text-center mt-8">
-            Don't have an account?
-            <br />
-            <a href="/ph-signup" className="text-accent underline text-xs">
-              Pharmacist Sign Up
-            </a>{" "}
-            |{" "}
-            <a href="/sign-up" className="text-accent underline text-xs">
-              Customer Sign Up
-            </a>
-          </p>
+            <p className="text-muted-foreground text-white text-center mt-8">
+              Don't have an account?
+              <br />
+              <a href="/ph-signup" className="text-accent underline text-xs">
+                Pharmacist Sign Up
+              </a>{" "}
+              |{" "}
+              <a href="/sign-up" className="text-accent underline text-xs">
+                Customer Sign Up
+              </a>
+            </p>
+          </div>
         </div>
-      </div>
+      </main>
       <Footer />
-    </>
+    </div>
   );
 };
 
---
+++ src/pages/Otp.jsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const primaryButtonClass =
+  "bg-x3 text-white px-4 py-2 mt-4 mx-4 rounded-full  hover:bg-opacity-90 font-semibold";
+
+const Otp = () => {
+  const [otp, setOtp] = useState("");
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    setEmail(sessionStorage.getItem("email"));
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    axios
+      .post(
+        `http://localhost:8081/api/v1/system-users/visitor/verify-email?email=${email}&otp=${otp}`
+      )
+      .then((res) => {
+        console.log(res);
+        sessionStorage.clear();
+        navigate("/login");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <div className="fixed text-sm inset-0 bg-x5 bg-opacity-50 flex justify-center items-center">
+      <div className="bg-white border-2 border-x1 p-6 rounded-xl w-80">
+        <h2 className="text-sm bg-x1 text-white font-semibold text-center border-spacing-x-1.5 mb-4 border p-2 rounded-full">
+          OTP Verification
+        </h2>
+        <h4 className="text-x1">
+          Your account verification code has been sent to your "
+          {email || "Example@mail.com"}"{" "}
+        </h4>
+        <password
+          type="text"
+          required
+          placeholder="Enter your OTP here"
+          className="border text-x1 border-x1 mt-4 hover:ring-1 rounded-full p-2 mb-2 w-full"
+          value={otp}
+          onChange={(e) => setOtp(e.target.value)}
+        />
+        <div className="flex justify-center">
+          <button
+            onClick={handleSubmit}
+            className={`ml-0 ${primaryButtonClass}`}
+          >
+            Confirm
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Otp;
